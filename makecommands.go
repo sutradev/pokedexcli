37fcae4b0d93bfef0361c0efa5930181f0519a3e
Isai <isaichaidez@gmail.com>
2024-08-12 15:58:23 -0700

    able to get locations, and page through them using map and mapb

--- makecommands.go
+++ makecommands.go
@@ -7,6 +7,10 @@ type clicommands struct {
 }
 
 func makeCommands() map[string]clicommands {
+	pokemonUrls := &PokemonUrls{
+		Next: new(string),
+		Prev: new(string),
+	}
 	return map[string]clicommands{
 		"help": {
 			name:        "help",
@@ -17,6 +21,16 @@ func makeCommands() map[string]clicommands {
 			name:        "exit",
 			description: "Exits program",
 			callback:    exitPokidex,
+		},
+		"map": {
+			name:        "map",
+			description: "Shows next 20 pokemon locations",
+			callback:    pokemonUrls.next20,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Goes back to the previous 20 pokemon locations",
+			callback:    pokemonUrls.prev20,
 		},
 	}
 }
